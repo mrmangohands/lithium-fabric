1f9a109112ad9c672d57ef07c5da76df44a0136a
mrmangohands <mrmangohands@protonmail.com>
2020-12-18 20:04:24 -0800

    fix: Fix portal optimizations for 1.16.1

--- src/main/java/me/jellysquid/mods/lithium/mixin/ai/poi/fast_portals/PortalForcerMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/ai/poi/fast_portals/PortalForcerMixin.java
@@ -2,16 +2,19 @@ package me.jellysquid.mods.lithium.mixin.ai.poi.fast_portals;
 
 import me.jellysquid.mods.lithium.common.world.interests.PointOfInterestStorageExtended;
 import net.minecraft.block.BlockState;
-import net.minecraft.class_5459;
+import net.minecraft.block.NetherPortalBlock;
+import net.minecraft.block.pattern.BlockPattern;
 import net.minecraft.server.world.ChunkTicketType;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.PortalForcer;
 import net.minecraft.world.poi.PointOfInterestStorage;
 import net.minecraft.world.poi.PointOfInterestType;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -29,9 +32,10 @@ public class PortalForcerMixin {
      * @author JellySquid
      * @reason Use optimized search for nearby points, avoid slow filtering, check for valid locations first
      */
+    @Nullable
     @Overwrite
-    public Optional<class_5459.class_5460> method_30483(BlockPos centerPos, boolean shrink) {
-        int searchRadius = shrink ? 16 : 128;
+    public BlockPattern.TeleportTarget getPortal(BlockPos centerPos, Vec3d vec3d, Direction direction, double x, double y, boolean canActivate) {
+        int searchRadius = 128;
 
         PointOfInterestStorage poiStorage = this.world.getPointOfInterestStorage();
         poiStorage.preloadChunks(this.world, centerPos, searchRadius);
@@ -41,13 +45,11 @@ public class PortalForcerMixin {
                 (poi) -> this.world.getBlockState(poi.getPos()).contains(Properties.HORIZONTAL_AXIS)
         );
 
-        return ret.flatMap((pos) -> {
-            BlockState state = this.world.getBlockState(pos);
+        return (BlockPattern.TeleportTarget)(ret.map((pos) -> {
+            this.world.getChunkManager().addTicket(ChunkTicketType.field_19280, new ChunkPos(pos), 3, pos);
 
-            this.world.getChunkManager().addTicket(ChunkTicketType.PORTAL, new ChunkPos(pos), 3, pos);
-
-            return Optional.of(class_5459.method_30574(pos, state.get(Properties.HORIZONTAL_AXIS), 21, Direction.Axis.Y, 21, (searchPos) ->
-                    this.world.getBlockState(searchPos) == state));
-        });
+            BlockPattern.Result result = NetherPortalBlock.findPortal(this.world, pos);
+            return result.getTeleportTarget(direction, pos, y, vec3d, x);
+        }).orElse(null));
     }
 }
