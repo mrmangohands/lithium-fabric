ab2069f6ae442e42ddf601971b34c115ed288d68
mrmangohands <mrmangohands@protonmail.com>
2020-12-18 17:58:30 -0800

    Revert "change: delete PistonHeadBlockMixin as vanilla includes an equivalent optimization since 1.16.2pre1"
    This reverts commit de4f806be00daecc0b0ef982d68d989a51804434.

--- src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
+++ src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
@@ -51,6 +51,7 @@ public class LithiumConfig {
         this.addMixinRule("block", true);
         this.addMixinRule("block.flatten_states", true);
         this.addMixinRule("block.moving_block_shapes", true);
+        this.addMixinRule("block.piston_shapes", true);
 
         this.addMixinRule("cached_hashcode", true);
 
